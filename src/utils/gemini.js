87b476af0f22b74526b9e4f5a3795df30ba0e276
PITSTOPSOFTCOM <lucasyurisilvabarbosa1@gmail.com>
2026-05-03 18:13:34 -0300

    ajustado modelo Gemini

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -33,7 +33,7 @@ function callGemini(prompt, attempt = 1) {
       }
     });
 
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+    const url  = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
     const req = https.request(url, {
       method: 'POST',
